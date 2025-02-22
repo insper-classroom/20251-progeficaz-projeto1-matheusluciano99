06be01046a4f5645a8d38d1155e8d2330c352d75
matheusluciano99 <matheusluciano99@gmail.com>
2025-02-22 13:21:23 -0300

    função submit_form funcionando

--- getit/servidor.py
+++ getit/servidor.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template_string, request, redirect
+from flask import Flask, render_template_string, request, redirect, url_for
+import sqlite3 as sql
 import views
 
 
@@ -10,16 +11,26 @@ app.static_folder = "static"
 
 @app.route("/")
 def index():
-    return render_template_string(views.index())
+    con = sql.connect("db_web.db")
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("select * from notes")
+    data = cur.fetchall()
+
+    return render_template_string(views.index(data), data=data)
 
 
 @app.route("/submit", methods=["POST"])
 def submit_form():
-    titulo = request.form.get("titulo")  # Obtém o valor do campo 'titulo'
-    detalhes = request.form.get("detalhes")  # Obtém o valor do campo 'detalhes'
-
-    views.submit(titulo, detalhes)
-    return redirect("/")
+    title = request.form.get("Title")
+    description = request.form.get("Description")
+    con = sql.connect("db_web.db")
+    cur = con.cursor()
+    cur.execute(
+        "insert into notes(Title, Description) values (?,?)", (title, description)
+    )
+    con.commit()
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
